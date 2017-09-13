8bf5e03828b41999d0d5329e83e510ce1c1789f2
esosebee <esosebee@unm.edu>
2017-09-13 13:39:48 -0600

    added functions to get attributes for triples

--- decision-tree.py
+++ decision-tree.py
@@ -35,7 +35,7 @@ def get_letter_indexes(sequence):
     return char_index_dict
 
 def get_sequence_chunks(sequence, n):
-    return [sequence[i:i+n] for i in range(0, len(sequence), 2)]
+    return [sequence[i:i+n] for i in range(0, len(sequence), n)]
 
 def split_sequence(sequence):
     return sequence[:len(sequence)/2], sequence[len(sequence)/2:]
@@ -57,6 +57,29 @@ def get_pair_index(sequence):
         pair_index_dict[pair] = [i for i, x in enumerate(seq_pairs) if x == pair]
     return pair_index_dict
 
+def get_pair_at_index(sequence, i_range):
+    index_pairs_dict = {}
+    for i in range(0, len(sequence)-1):
+        index_pairs_dict[i] = sequence[i:i+2]
+    return index_pairs_dict
+
+def get_triples_position(sequence):
+    triples_pos_dict = {}
+    triples = get_sequence_chunks(sequence, 3)
+    
+    # Gets indices of values in triples
+    for t in triples:
+        # triples_pos_dict[t] = [(i*3,i*3+1,i*3+2) for i, x in enumerate(triples) if x == t]
+        triples_pos_dict[t] = [i*3 for i, x in enumerate(triples) if x == t]
+    return triples_pos_dict
+
+def get_triple_at_index(sequence, i_range):
+    index_triples_dict = {}
+    for i in range(0, len(sequence)-2):
+        index_triples_dict[i] = sequence[i:i+3]
+    return index_triples_dict
+
+# TODO: get triples and their position
 def get_attributes(data):
     attributes = {}
     attributes['id'] = int(data[0])
@@ -65,7 +88,11 @@ def get_attributes(data):
     attributes['letter_frequencies'] = get_letter_frequencies(data[1])
     attributes['letter_index'] = get_letter_indexes(data[1])
     attributes['pair_frequencies'] = get_pair_frequencies(data[1])
-    attributes['pair_index'] = get_pair_index(data[1])
+    attributes['pair_at_index'] = get_pair_at_index(data[1], range(0, len(data[1])))
+    attributes['triple_at_index'] = get_triple_at_index(data[1], range(0, len(data[1])))
+    # attributes['pair_position'] = get_pair_index(data[1])
+    # attributes['triples_position'] = get_triples_position(data[1])
+    
     return attributes
 
 if __name__ == '__main__':
@@ -77,17 +104,17 @@ if __name__ == '__main__':
     training_data = load_dataset(training_filename)
     testing_data = load_dataset(testing_filename)
 
-    for t in training_data:
-        training_attributes.append(get_attributes(t))
+    # for t in training_data:
+    #     training_attributes.append(get_attributes(t))
 
 
     
-    '''test_data = ['1995', 'GCTGAGGCCTGGCTCTCTCCCTCCCCACAGGGTGCCCGGTACGTGTGGAACCGCACTGAG', 'IE']
+    test_data = ['1995', 'GCTGAGGCCTGGCTCTCTCCCTCCCCACAGGGTGCCCGGTACGTGTGGAACCGCACTGAG', 'IE']
     attributes = get_attributes(test_data)
-    for key in attributes:
-        print key
-        print attributes[key]
-        print '\n' '''
+    # for key in attributes:
+    #     print key
+    #     print attributes[key]
+    #     print '\n' 
 
 
 
