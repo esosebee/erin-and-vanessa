bb6a8826cc702891cd0c35d0378b34bf317a622c
esosebee <esosebee@unm.edu>
2017-09-26 14:10:28 -0600

    Added print_tree function

--- src/build_tree.py
+++ src/build_tree.py
@@ -4,43 +4,41 @@ class Tree:
     dataset = None # The dataset that was tested for this node
     remaining_attributes_keys = None
     target_attr =  None # The target attribute to be tested for (a.k.a. 'boundary')
-    labels = None # Target outcomes (a.k.a. 'IE', 'EI', 'N')
     node_feature = None # Feature that we're splitting on
     children = None # A list of this node's child nodes
     parent = None # Parent of the current node
     decision = None # The final prediction of the branch of the tree (only as value at leaf nodes: IE, EI, N)
+    is_leaf = None # Marks if a tree is a leaf or not
     depth = None # The depth of the node in the tree
 
-    def __init__(self, dataset, remaining_attributes_keys, target_attr, labels, node_feature, children, parent, decision, depth):
+    def __init__(self, dataset, remaining_attributes_keys, target_attr, node_feature, children, parent, decision, is_leaf, depth):
         '''
         Create decision tree node.
         '''
         self.dataset = dataset
         self.remaining_attributes_keys = remaining_attributes_keys
         self.target_attr = target_attr 
-        self.labels = labels
         self.node_feature = node_feature 
         self.children = children 
         self.parent = parent
         self.decision = decision
+        self.is_leaf = is_leaf
         self.depth = depth
 
         # Begin building tree
-        self.build_decision_tree(dataset, remaining_attributes_keys, target_attr, labels, depth)
+        self.build_decision_tree(dataset, remaining_attributes_keys, target_attr, is_leaf, depth)
+        return
 
-    def build_decision_tree(self, dataset, remaining_attributes_keys, target_attr, labels, depth):
+    def build_decision_tree(self, dataset, remaining_attributes_keys, target_attr, is_leaf, depth):
         '''
         Recursively build the decision tree.
         '''
-        # print 'dataset length: ', len(dataset[0])
-        # print 'remaining_attributes_keys length: ', len(remaining_attributes_keys)
-        # raw_input('Press any key to continue...')
-
         # Remaining boundary values
-        target_values = infogain.find_values_of_attribute(dataset, target_attr)
+        target_values = list(infogain.find_values_of_attribute(dataset, target_attr))
 
-        # At leaf node - stop recursing
+        # At leaf node: stop recursing
         if len(dataset) == 1 or len(remaining_attributes_keys) == 1 or len(target_values) == 1:
+            self.is_leaf = True    
             self.decision = dataset[0][target_attr]
             return 
 
@@ -54,39 +52,8 @@ class Tree:
         self.children = []
         for val in best_feature_values:
             child_dataset = infogain.slice_of_data(dataset, best_feature, val)
-            # print 'child_dataset length: ', len(child_dataset)
-            # print child_dataset
-            self.children.append(Tree(child_dataset, child_remaining_attributes_keys, target_attr, labels, best_feature, None, self, None, depth+1))
+            self.children.append(Tree(child_dataset, child_remaining_attributes_keys, target_attr, best_feature, None, self, None, False, depth+1))
 
-        # TODO: print tree functionality
-
-        # default_prediction = infogain.get_default_prediction(dataset, target_attr) # Get default feature
-        # if len(dataset[0]) == 1 or target_attr not in dataset[0]: # Stop splitting, there are no more features
-        #     # Get decision 
-        #     self.decision = defaut_prediction 
-        #     return 
-
-        # Get unique attribute values 
-        # unique_values = [] 
-        # attribute_keys = dataset[0].keys() # All features to be tested on
-        # for key in attribute_keys:
-        #     unique_values.append(infogain.find_unique_values(dataset, key))
-        # unique_values = list(set(infogain.flatten_list(unique_values)))
-        
-
-        
-        
-        # Remove selected feature from dataset for children nodes 
-        # child_dataset = infogain.remove_attribute_from_list(dataset, self.node_feature)
-
-        # Remove selected feature from unique values
-        # child_unique_values = [x for x in remaining_attributes_keys if x != self.node_feature]
-        
-        # Begin recursing for children 
-        # self.children = []
-        # for value in unique_values:
-        #     child_labels = labels[:] # TODO: figure out how to split labels
-        #     self.children.append(Tree(infogain.split_dataset(dataset, value), target_attr, child_labels, value, None, self, None, depth+1))
 
 
 
--- src/main.py
+++ src/main.py
@@ -1,9 +1,24 @@
 import get_data
 import information_gain as infogain
-import chi_square_test as chi 
 from build_tree import Tree
 
+import chi_square_test as chi 
+
 DEBUG = True
+
+def print_tree(tree, indent):
+    indentation = indent * ' '
+    child_indentation = (indent + 2) * ' '
+    if tree.is_leaf == False:
+        print indentation + 'Parent: ' + str(tree.node_feature)
+        for child in tree.children:
+            if child.is_leaf == False:
+                print child_indentation + 'Child: ' + str(child.node_feature)
+                print_tree(child, indent+2)
+            else:
+                print child_indentation + 'Decision parent: ' + str(child.parent.node_feature)
+                print child_indentation + 'Decision: ' + str(child.decision)
+
 
 if __name__ == '__main__':
     training_filename = 'training.csv'
@@ -17,14 +32,18 @@ if __name__ == '__main__':
         training_attributes_list.append(get_data.get_attributes(t))
 
     target_attr = 'boundary'
+    attribute_keys = training_attributes_list[0].keys()
+    dtree = Tree(training_attributes_list, attribute_keys, target_attr, None, None, None, None, False, 0)
+    if DEBUG:
+        print_tree(dtree, 0)
 
     # Test on partition of data
-    if DEBUG:
-        test_data = training_data
-        test_attributes = training_attributes_list
-        attribute_keys = test_attributes[0].keys()
-        labels = list(infogain.find_values_of_attribute(test_attributes, target_attr))
-        tree = Tree(test_attributes, attribute_keys, target_attr, labels, None, None, None, None, 0)
+    # if DEBUG:
+    #     test_data = training_data
+    #     test_attributes = training_attributes_list
+    #     attribute_keys = test_attributes[0].keys()
+    #     labels = list(infogain.find_values_of_attribute(test_attributes, target_attr))
+    #     tree = Tree(test_attributes, attribute_keys, target_attr, labels, None, None, None, None, 0)
         # chi.chi_square_test(test_attributes, target_attr, labels, 0.05)
 
     # test_data = ['1995', 'GCTGAGGCCTGGCTCTCTCCCTCCCCACAGGGTGCCCGGTACGTGTGGAACCGCACTGAG', 'IE']
