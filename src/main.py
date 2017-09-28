b1c033ebb75a8a1ee49e5d42d9da3ffcd6c91421
esosebee <esosebee@unm.edu>
2017-09-28 12:37:33 -0600

    fixed classifier and wrote submission file writer

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
-import os
+import csv, os
 import get_data
 import information_gain as infogain
-import build_tree
 from build_tree import Tree
 
 import chi_square_test as chi 
@@ -45,11 +44,15 @@ def remove_key(dicts, key):
     return d
 
 def classify(test_data, tree):
+    '''
+    Classifies the given data by recursively traversing the decision tree created with 
+    the training data. At each node, the feature label and the feature value are 
+    compared to what is present in test_data. If the test_data contains the value, 
+    then this function recurses on that node.
+    '''
     # At leaf node 
     if tree.children is None:
-        final_str = str(test_data['id']) + ',' + str(tree.decision)
-        # print final_str
-        return final_str
+        return test_data['id'],tree.decision
 
     children_check = False
     for child in tree.children:
@@ -57,14 +60,25 @@ def classify(test_data, tree):
         child_value = child.node_feature_value 
         children_check = children_check or (test_data[child_label] == child_value)
         if test_data[child_label] == child_value:
-            classify(test_data, child)
+            return classify(test_data, child)
 
     # If a leaf node can't be reached, return the default prediction
     if children_check == False:
-        final_str = str(test_data['id']) + ',' + str(tree.default_prediction)
-        # print final_str 
-        return final_str
+        return test_data['id'], tree.default_prediction
 
+def write_to_csv(headers, data):
+    '''
+    Writes prediction data into a CSV file. 
+    '''
+    filename = 'submission.csv'
+    for d in data:
+        print d
+    with open(filename, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
+        for d in data:
+            writer.writerow(d)
+        f.close()
 
 if __name__ == '__main__':
     training_filename = 'training.csv'
@@ -89,18 +103,17 @@ if __name__ == '__main__':
     testing_attributes_list = []
     for t in testing_data:
         testing_attributes_list.append(get_data.get_testing_attributes(t))
-
-    # Get all possible predictions
-    # target_values = infogain.find_unique_values(training_attributes_list, target_attr)
     
     # Classify testing data with decision tree
     predictions = []
     for t in testing_attributes_list:
-        print classify(t, dtree)
-        # build_tree.classify(t, dtree)
-        # print build_tree.classify(t, dtree)
-        # predictions.append(build_tree.classify(t, dtree))
-    # print predictions
+        predictions.append(classify(t, dtree))
+
+    # Write predictions to file 
+    headers = ['id','class']
+    write_to_csv(headers, predictions)
+    
+
     
 
 
